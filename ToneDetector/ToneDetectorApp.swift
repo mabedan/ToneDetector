aa648e6f91ea862caf682184accc367fe9c7b3d1
mesfahanisimscale <mesfahani@simscale.com>
2025-10-29 13:25:28 +0100

    Initial Commit

---
+++ ToneDetector/ToneDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToneDetectorApp.swift
+//  ToneDetector
+//
+//  Created by Mahdi Esfahani on 29.10.25.
+//
+
+import SwiftUI
+
+@main
+struct ToneDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
